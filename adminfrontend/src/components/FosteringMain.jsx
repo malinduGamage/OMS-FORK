3931392694ed0ef0d4a1cc1d2b7f9b220b54e97a
Savindu Gunasekara <savindugunasekara@gmail.com>
2024-08-21 23:21:40 +0530

    chatbot added

--- adminfrontend/src/components/FosteringMain.jsx
+++ adminfrontend/src/components/FosteringMain.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'  
 import UserApplicationList from './UserApplicationList';
+import ChatBot from './ChatBot';
 
 const buttonClasses = 'focus:outline-none text-black bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-orange-300 rounded-lg text-2xl px-5 py-2.5 me-2 mb-2 dark:focus:ring-orange-800';
 
@@ -17,6 +18,8 @@ export default function FosteringMain() {
     <div className="flex justify-center">
         <Link to="/mycases" className={`p-2 ${buttonClasses} z-0`}>Ongoing Cases</Link>
     </div>
+
+   <div><ChatBot/></div>
 
 
 
